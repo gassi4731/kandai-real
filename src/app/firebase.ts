aeca53156708e10237997430575a3783a4d1f9ed
txxxxc <tomoya113.code@gmail.com>
2024-03-27 09:50:33 +0900

    fix: error

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
@@ -16,7 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const storage = getStorage(app, "gs://kandai-real.appspot.com");
 export const db = getFirestore(app);
 
